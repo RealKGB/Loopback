cca8418270e3f056478a97dd268a112a520e8800
WhitetailAni <joemama@thanos.lol>
2023-04-24 21:38:41 -0500

    Initial Commit

---
+++ Loopback/LoopbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopbackApp.swift
+//  Loopback
+//
+//  Created by RealKGB on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LoopbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
