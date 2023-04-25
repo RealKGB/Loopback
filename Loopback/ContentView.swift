3b69917c48dd3d993f9df18caabb267f5b5aef22
WhitetailAni <joemama@thanos.lol>
2023-04-24 22:07:31 -0500

    this was made in 15 minutes

--- Loopback/ContentView.swift
+++ Loopback/ContentView.swift
@@ -6,21 +6,48 @@
 //
 
 import SwiftUI
+import AVKit
+import CoreMotion
 
 struct ContentView: View {
+
+    @State var dataDirectory: String = ""
+    let gyro = CMMotionManager()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            let videoURL = URL(fileURLWithPath: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path + "/video.mp4")
+            VideoPlayer(player: AVPlayer(url: videoURL))
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    //video player
+                    let player = AVPlayer(url: videoURL)
+                    let controller = AVPlayerViewController()
+                    controller.player = player
+                    controller.showsPlaybackControls = false
+                    NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (_) in
+                        player.seek(to: .zero)
+                        player.play()
+                    }
+                    UIApplication.shared.windows.first?.rootViewController?.present(controller, animated: true, completion: {
+                        controller.player?.play()
+                    })
+                    print(videoURL)
+                    
+                    //gyro
+                    if gyro.isGyroAvailable {
+                        gyro.gyroUpdateInterval = 0.1
+                        gyro.startGyroUpdates(to: .main) { gyroData, error in
+                            guard let gyroData = gyroData else { return }
+                            let gyroMagnitude = sqrt(gyroData.rotationRate.x * gyroData.rotationRate.x +
+                                                      gyroData.rotationRate.y * gyroData.rotationRate.y +
+                                                      gyroData.rotationRate.z * gyroData.rotationRate.z)
+                            if gyroMagnitude > 2.0 {
+                                exit(69)
+                            }
+                        }
+                    }
+                }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
